2ac42794eea6207b21cb6745b92dac99d2ca0079
Minh Ngoc Pham <ngocminh7200@gmail.com>
2022-03-09 00:24:57 +0100

    Debug

--- src/main/java/Solver.java
+++ src/main/java/Solver.java
@@ -33,12 +33,17 @@ public class Solver {
 			e.printStackTrace();
 		}
 		
+		HashMap<String, HashMap<String, Product>> trial = new HashMap<String, HashMap<String, Product>>();
+		trial.put("Poppenverzorgingsproduct", dt.get(0).get("Poppenverzorgingsproduct"));
+		trial.put("Speelgoedwatersproeier", dt.get(0).get("Speelgoedwatersproeier"));
+		
 		// Optimize the problem.
 		String[] sizes = {"XXXS", "XXS", "XS", "S", "M", "L", "XL", "XXL", "XXXL"};
 				
 		try
 		{
-			solve(52, sizes, dt.get(0));
+			solve(52, sizes, trial);
+//			solve(52, sizes, dt.get(0));
 		}
 		catch (IloException e)
 		{
@@ -100,13 +105,16 @@ public class Solver {
 						cplex.addLe(x[t][i][s][0], cplex.prod(cap0, cplex.sum(1, cplex.negative(y[i]))), "Constraints on goods allocation");
 						cplex.addLe(x[t][i][s][1], cplex.prod(cap1, y[i]), "Constraints on goods allocation");
 						if (t + 1 != T) {
-							cplex.addEq(cplex.sum(u[t + 1][i][s], z[t][i][s]), cplex.sum(x[t][i][s][0], x[t][i][s][1]));
+							cplex.addEq(cplex.sum(u[t + 1][i][s], z[t][i][s]), cplex.sum(x[t][i][s][0], x[t][i][s][1]), "Inventory at the beginning of the period");
 						}
 					}
 				}
 			}
 		}
-		cplex.addMinimize(objExpr);
+		cplex.addMaximize(objExpr);
+		
+		// Export model
+		cplex.exportModel("Model.lp");
 		
 		// Solve the model.
 
@@ -120,12 +128,15 @@ public class Solver {
 			for (int t = 0; t < T; t++)	{
 				for (int i = 0; i < n; i ++) {
 					for (int s = 0; s < size; s++) {
-						if (cplex.getValue(x[t][i][s][0]) >= 0.5) {
-							System.out.println("We store " + chunkNames.get(i) + " in small warehouse with amount " + cplex.getValue(x[t][i][s][0]) + " of size " + sizes[s] + " in week " + t);
-						}
-						else {
-							System.out.println("We store " + chunkNames.get(i) + " in big warehouse with amount " + cplex.getValue(x[t][i][s][0]) + " of size " + sizes[s] + " in week " + t);
-							
+						HashMap<String, Product> chunk = data.get(chunkNames.get(i));
+						Product prod = chunk.get(sizes[s]);
+						if (prod != null) {
+							if (cplex.getValue(y[i]) <= 0.5) {
+								System.out.println("We store " + chunkNames.get(i) + " in small warehouse with amount " + cplex.getValue(x[t][i][s][0]) + " of size " + sizes[s] + " in week " + t);
+							}
+							else {
+								System.out.println("We store " + chunkNames.get(i) + " in big warehouse with amount " + cplex.getValue(x[t][i][s][0]) + " of size " + sizes[s] + " in week " + t);	
+							}
 						}
 					}
 				}
