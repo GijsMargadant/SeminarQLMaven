e44276afb33c5cbaa95e3decb6573ef16327f3ed
GijsMargadant <86891988+GijsMargadant@users.noreply.github.com>
2022-03-08 12:54:34 +0100

    Made the file paths point to the data in the dataFolder within the project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,12 @@ import java.util.ArrayList;
 public class Main {
 
 	public static void main(String[] args) {
-		File data = new File("C:\\Users\\gijsm\\Documents\\DOCUMENTEN\\School\\SeminarCaseStudy\\OriginalData\\dataset.xlsx");
-		File relevanceScore = new File("C:\\Users\\gijsm\\Documents\\DOCUMENTEN\\School\\SeminarCaseStudy\\OriginalData\\EUR_BusinessCase_Chunk_RelevanceScore_V2.xlsx");
-		File warehouseCost = new File("C:\\Users\\gijsm\\Documents\\DOCUMENTEN\\School\\SeminarCaseStudy\\OriginalData\\EUR_BusinessCase_Sizegroup_Costs.xlsx");
+		// Since I've created a dataFile folder in the project containing all xlsx files, you can access them
+		// in this way. This probably only works if you have eclipse and GitHub linked. Otherwise, you should
+		// use the file paths from your own PC.
+		File data = new File(".\\dataFiles\\dataset.xlsx");
+		File relevanceScore = new File(".\\\\dataFiles\\EUR_BusinessCase_Chunk_RelevanceScore_V2.xlsx");
+		File warehouseCost = new File(".\\\\dataFiles\\EUR_BusinessCase_Sizegroup_Costs.xlsx");
 		
 		try {
 			CustomDataReader cdm = new CustomDataReader(data, relevanceScore, warehouseCost);
