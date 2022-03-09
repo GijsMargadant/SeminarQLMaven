e0ae7f7075216e3c3dfc8cddd5a19620c6c70a85
florishaverman <46428529+florishaverman@users.noreply.github.com>
2022-03-09 15:12:24 +0100

    Set up different file paths

--- src/main/java/CustomDataReader.java
+++ src/main/java/CustomDataReader.java
@@ -20,12 +20,24 @@ public class CustomDataReader {
 	public static void main(String[] args) {
 		long tic = System.currentTimeMillis();
 		
+		boolean onWindows = false;
+		
 		// Since I've created a dataFile folder in the project containing all xlsx files, you can access them
 		// in this way. This probably only works if you have eclipse and GitHub linked. Otherwise, you should
 		// use the file paths from your own PC.
-		File data = new File("./dataFiles/dataset.xlsx");
-		File relevanceScore = new File("./dataFiles/EUR_BusinessCase_Chunk_RelevanceScore_V2.xlsx");
-		File warehouseCost = new File("./dataFiles/EUR_BusinessCase_Sizegroup_Costs.xlsx");
+		File data;
+		File relevanceScore;
+		File warehouseCost;
+		if (onWindows) {
+			data = data = new File(".\\dataFiles\\dataset.xlsx");
+			relevanceScore = new File(".\\dataFiles\\EUR_BusinessCase_Chunk_RelevanceScore_V2.xlsx");
+			warehouseCost = new File(".\\dataFiles\\EUR_BusinessCase_Sizegroup_Costs.xlsx");
+		}else {
+			data = new File("./dataFiles/dataset.xlsx");
+			relevanceScore = new File("./dataFiles/EUR_BusinessCase_Chunk_RelevanceScore_V2.xlsx");
+			warehouseCost = new File("./dataFiles/EUR_BusinessCase_Sizegroup_Costs.xlsx");
+		}
+		
 		
 		try {
 			CustomDataReader cdm = new CustomDataReader(data, relevanceScore, warehouseCost);
